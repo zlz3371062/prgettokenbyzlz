696e3cdefa821239cfbfb8a8efcfcba2d1b5c532
zlz <438580470@qq.com>
2016-03-30 21:29:27 +0800

    seconed

---
+++ src/com/example/prgettokenbyzlz/Snippet.java
@@ -0,0 +1,7 @@
+package com.example.prgettokenbyzlz;
+
+public class Snippet {
+	public static void main(String[] args) {
+	}
+}
+
---
+++ src/com/example/prgettokenbyzlz/aa.java
@@ -0,0 +1,7 @@
+package com.example.prgettokenbyzlz;
+
+public class aa {
+	public static void main(String[] args) {
+		System.out.println();
+	}
+}
